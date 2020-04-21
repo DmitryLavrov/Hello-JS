0e002182894d58d02d2624b8148b591a36f89948
DmitryLavrov <d3236562@mail.ru>
2020-04-21 22:54:37 +0300

    filterRange(arr, a, b) -- Фильтрация по диапазону

--- hello.js
+++ hello.js
@@ -10,3 +10,23 @@ function camelize(str) {
     )
     .join(''); // соединяет ['my', 'Long', 'Word'] в 'myLongWord'
 }
+
+function filterRange(arr, a, b) {
+  return arr
+    .filter(item => item >= a && item <= b);
+}
+
+function filterRangeInPlace(arr, a, b) {
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] < a || arr[i] > b) {
+      arr.splice(i, 1);
+      i--;
+    }
+  }
+}
+
+let arr = [5, 3, 2, 3, 8, 7, 6, 5, 1];
+
+filterRangeInPlace(arr, 1, 4); // удалены числа вне диапазона 1..4
+
+alert(arr); // [3, 1]
