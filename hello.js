21d11e518a62202c067d63d445aa4a3eb61cc616
DmitryLavrov <d3236562@mail.ru>
2020-04-21 10:55:49 +0300

    Функция camelize(str)

--- hello.js
+++ hello.js
@@ -1,60 +1,12 @@
 'use strict';
 
-function getMaxSubSum(arr) {
-  let maxSum = 0;
-  let partialSum = 0;
-
-  for (let item of arr) { // для каждого элемента массива
-    partialSum += item; // добавляем значение элемента к partialSum
-    maxSum = Math.max(maxSum, partialSum); // запоминаем максимум на данный момент
-    if (partialSum < 0) partialSum = 0; // ноль если отрицательное
-  }
-
-  return maxSum;
+function camelize(str) {
+  return str
+    .split('-') // разбивает 'my-long-word' на массив ['my', 'long', 'word']
+    .map(
+      // Переводит в верхний регистр первые буквы всех элементом массива за исключением первого
+      // превращает ['my', 'long', 'word'] в ['my', 'Long', 'Word']
+      (item, index) => index == 0 ? item : item[0].toUpperCase() + item.slice(1)
+    )
+    .join(''); // соединяет ['my', 'Long', 'Word'] в 'myLongWord'
 }
-
-alert( getMaxSubSum([-1, 2, 3, -9]) ); // 5
-alert( getMaxSubSum([-1, 2, 3, -9, 11]) ); // 11
-alert( getMaxSubSum([-2, -1, 1, 2]) ); // 3
-alert( getMaxSubSum([100, -9, 2, -3, 5]) ); // 100
-alert( getMaxSubSum([1, 2, 3]) ); // 6
-alert( getMaxSubSum([-1, -2, -3]) ); // 0
-
-
-describe("getMaxSubSum", function() {
-  it("maximal subsum of [1, 2, 3] equals 6", function() {
-    assert.equal(getMaxSubSum([1, 2, 3]), 6);
-  });
-
-  it("maximal subsum of [-1, 2, 3, -9] equals 5", function() {
-    assert.equal(getMaxSubSum([-1, 2, 3, -9]), 5);
-  });
-
-  it("maximal subsum of [-1, 2, 3, -9, 11] equals 11", function() {
-    assert.equal(getMaxSubSum([-1, 2, 3, -9, 11]), 11);
-  });
-
-  it("maximal subsum of [-2, -1, 1, 2] equals 3", function() {
-    assert.equal(getMaxSubSum([-2, -1, 1, 2]), 3);
-  });
-
-  it("maximal subsum of [100, -9, 2, -3, 5] equals 100", function() {
-    assert.equal(getMaxSubSum([100, -9, 2, -3, 5]), 100);
-  });
-
-  it("maximal subsum of [] equals 0", function() {
-    assert.equal(getMaxSubSum([]), 0);
-  });
-
-  it("maximal subsum of [-1] equals 0", function() {
-    assert.equal(getMaxSubSum([-1]), 0);
-  });
-
-  it("maximal subsum of [-1, -2] equals 0", function() {
-    assert.equal(getMaxSubSum([-1, -2]), 0);
-  });
-
-  it("maximal subsum of [2, -8, 5, -1, 2, -3, 2] equals 6", function() {
-    assert.equal(getMaxSubSum([2, -8, 5, -1, 2, -3, 2]), 6);
-  });
-});
